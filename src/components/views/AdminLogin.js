d2e0e3e048d9844d9e4b9214b0a533927e4c6589
Mely <95911782+Meleac@users.noreply.github.com>
2023-03-30 09:47:01 +0200

    roughly modify AdminLogin.js

---
+++ src/components/views/AdminLogin.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import {api, handleError} from 'helpers/api';
+import User from 'models/User';
+import {useHistory} from 'react-router-dom';
+import {Button} from 'components/ui/Button';
+import 'styles/views/Login.scss';
+import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+
+/*
+It is possible to add multiple components inside a single file,
+however be sure not to clutter your files with an endless amount!
+As a rule of thumb, use one file per component and only add small,
+specific components that belong to the main one in the same file.
+ */
+const FormField = props => {
+    return (
+            <div className="login field">
+                <input
+                        className="login input"
+                        placeholder="enter your username.."
+                        value={props.value}
+                        onChange={e => props.onChange(e.target.value)}
+                />
+            </div>
+    );
+};
+
+FormField.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+const Login = props => {
+    const history = useHistory();
+    const [username, setUsername] = useState(null);
+
+    /*const goBack = {
+        history.push();
+    }*/ //push back to homepage
+
+    const doLogin = async () => {
+        try {
+            const requestBody = JSON.stringify({username});
+            const response = await api.post('/users', requestBody);
+
+            // Get the returned user and update a new object.
+            const user = new User(response.data);
+
+            // Store the token into the local storage.
+            localStorage.setItem('token', user.token);
+
+            // Login successfully worked --> navigate to the route /game in the GameRouter
+            history.push(`/game`);
+        } catch (error) {
+            alert(`Something went wrong during the login: \n${handleError(error)}`);
+        }
+    };
+
+    return (
+            <BaseContainer>
+                <div className="login container">
+                    <div className="login form">
+                        <FormField
+                                value={username}
+                                onChange={un => setUsername(un)}
+                        />
+                        <div className="login button-container">
+                            <Button
+                                    disabled={!username}
+                                    width="100%"
+                                    onClick={() => doLogin()}
+                            >
+                                ENTER
+                            </Button>
+                            <Button
+                                    width="100%"
+                                    onClick={() => goBack()}
+                            >
+                                BACK
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </BaseContainer>
+    );
+};
+
+/**
+ * You can get access to the history object's properties via the withRouter.
+ * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
+ */
+export default Login;
