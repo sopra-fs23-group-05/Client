8d859edb343d6ba6f80c6211429ec46f86e6d486
Mely <95911782+Meleac@users.noreply.github.com>
2023-04-06 16:35:21 +0200

    make little changes in Game.js

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -28,29 +28,17 @@ const Game = () => {
   // keep its value throughout render cycles.
   // a component can have as many state variables as you like.
   // more information can be found under https://reactjs.org/docs/hooks-state.html
-  const [users, setUsers] = useState(null);
+  const [teamGuesser, setTeamGuesser] = useState(null);
+  const [clueGiver, setClueGiver] = useState(null);
+  const [teamBuzzer, setTeamBuzzer] = useState(null);
+  const [user, setUser] = useState(null);
 
-  const logout = () => {
-    localStorage.removeItem('token');
-    history.push('/login');
-  }
-
-  // the effect hook can be used to react to change in your component.
-  // in this case, the effect hook is only run once, the first time the component is mounted
-  // this can be achieved by leaving the second argument an empty array.
-  // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
   useEffect(() => {
-    // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
     async function fetchData() {
       try {
         const response = await api.get('/users');
-
-        // delays continuous execution of an async operation for 1 second.
-        // This is just a fake async call, so that the spinner can be displayed
-        // feel free to remove it :)
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        // Get the returned users and update the state.
         setUsers(response.data);
 
         // This is just some data for you to see what is available.
