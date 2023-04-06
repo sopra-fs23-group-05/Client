c73d1daa8ff1d087bb001c93c1c6fe589fd456db
Mely <95911782+Meleac@users.noreply.github.com>
2023-04-06 16:40:42 +0200

    add fields for two teams in Lobby.js

--- src/models/Lobby.js
+++ src/models/Lobby.js
@@ -6,6 +6,9 @@ class Lobby {
         this.accessCode = null;
         this.lobbyLeader = null;
         this.aSettings = null;
+        this.lobbyUsers = null;
+        this.team1 = null;
+        this.team2 = null;
         Object.assign(this, data);
     }
 }
