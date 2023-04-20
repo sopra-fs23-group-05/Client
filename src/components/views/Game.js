3b8dcd122cecb94fda2bb5b3c8afc6c168e71bed
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-04-20 22:09:02 +0200

    secure connection to ws #63

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -21,7 +21,7 @@ export default function Game(){
     useEffect(() => {
         console.log('Opening WebSocket');
         // Activate the following line for deployment.
-        webSocket.current = new WebSocket('ws://sopra-fs23-group-05-server.oa.r.appspot.com/chat');
+        webSocket.current = new WebSocket('wss://sopra-fs23-group-05-server.oa.r.appspot.com/chat');
         // Activate the following line for local testing.
         // webSocket.current = new WebSocket('ws://localhost:8080/chat');
         const openWebSocket = () => {
