ccf0e76f2eedb21fc9406b7e4f8980f8e9f63042
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-04-29 18:10:20 +0200

    tag

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -236,8 +236,6 @@ export default function Game() {
 
     /* This code is iterating over an array of chatMessages and returning
     * a new array of ListItem components
-    *
-    *
      */
     const listChatMessages = chatMessages.map((ChatMessage, index) =>
             <Box key={index}
