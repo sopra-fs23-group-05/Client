e6f188dcf566bbe0721faaa40bd682b36304fb78
tom7689 <tomkaspar.meier@uzh.ch>
2023-05-04 15:25:03 +0200

    guards if your logged in you cant access a game or lobby

--- src/components/routing/routeProtectors/LobbyGuard.js
+++ src/components/routing/routeProtectors/LobbyGuard.js
@@ -1,10 +1,17 @@
 import {Redirect} from "react-router-dom";
 import PropTypes from "prop-types";
+import {api} from "../../../helpers/api";
 
-export const LobbyGuard = props => {
-    if (localStorage.getItem("token")) {
+export const LobbyGuard =  props => {
+    const accessCode = localStorage.getItem('lobbyAccessCode');
+
+    const url = window.location.href;
+    const urlSplit = url.split("/");
+
+    if (urlSplit[3] === "lobbies" && urlSplit[4] === accessCode) {
         return props.children;
     }
+
     return <Redirect to="/homepage"/>;
 };
 
--- src/components/routing/routeProtectors/PreGameGuard.js
+++ src/components/routing/routeProtectors/PreGameGuard.js
@@ -2,7 +2,12 @@ import {Redirect} from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const PreGameGuard = props => {
-    if (localStorage.getItem("token")) {
+    const accessCode = localStorage.getItem('lobbyAccessCode');
+
+    const url = window.location.href;
+    const urlSplit = url.split("/");
+
+    if (urlSplit[3] === "games" && urlSplit[4] === accessCode) {
         return props.children;
     }
     return <Redirect to="/homepage"/>;
