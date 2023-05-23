ba426f3657b3c658257259cae2763edc47bfa882
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-05-23 23:49:27 +0200

    error message user login

--- src/components/views/UserLogin.js
+++ src/components/views/UserLogin.js
@@ -24,7 +24,9 @@ const UserLogin = () => {
 
   const [errorMessage, setErrorMessage] = useState("");
   const [givenAccessCode, setGivenAccessCode] = useState(accessCodeURL);
-  const [errorAlertVisible, setErrorAlertVisible] = useState(false);
+  const [errorAlertVisible, setErrorAlertVisible] = useState(false); // State for error alert
+  const [loginButtonDisables, disableLoginButton] = useState(true); // State for login button
+  const [errorContent, setErrorContent] = useState(null);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -32,6 +34,38 @@ const UserLogin = () => {
   const handleAccessCodeChange = (event) => {
     setGivenAccessCode(event.target.value);
   }
+
+
+  useEffect(() => {
+    // Enable login button if requirements are met
+    if (username !== '') {
+      disableLoginButton(false);
+    }
+    const accessCodeNumber = parseInt(givenAccessCode);
+
+    // Hide error message if access code is empty
+    if (!isNaN(accessCodeNumber) || givenAccessCode === '') {
+      setErrorContent(null);
+    }
+
+    // Disable login button if access code is not a number
+    if (isNaN(accessCodeNumber) && givenAccessCode !== '') {
+      disableLoginButton(true);
+      setErrorContent(
+          <Typography variant="h5" className="title" style={{
+            fontSize: '16px',
+            marginTop: '-16px',
+            marginBottom: '4px'
+          }}>Please enter an integer.</Typography>
+      );
+    } else if (givenAccessCode === '') {
+      disableLoginButton(true);
+    }
+    // Disable login button if username is empty
+    if (username === '') {
+      disableLoginButton(true);
+    }
+  }, [givenAccessCode, username]);
 
   const playSound = (soundFile) => {
     const audio = new Audio(soundFile);
@@ -81,12 +115,11 @@ const UserLogin = () => {
       history.push(`/lobbies/${lobby.accessCode}`);
 
     } catch (error) {
-      console.log(error);
         setErrorMessage(error);
-      setErrorAlertVisible(true);
+      setErrorAlertVisible(true); // Show the error alert
       setTimeout(() => {
-        setErrorAlertVisible(false);
-      }, 8000);
+        setErrorAlertVisible(false); // Hide the error alert after 5 seconds
+      }, 5000);
     }
   };
 
@@ -101,6 +134,7 @@ const UserLogin = () => {
           value={givenAccessCode}
           onChange={handleAccessCodeChange}
         />
+        {errorContent}
         <TextField className="custom-outlined-text-field"
           sx={{ marginBottom: '20px' }}
           label='Username'
@@ -118,16 +152,16 @@ const UserLogin = () => {
           <Button variant="contained"
             className="buttonLogin"
             onClick={() => doLogin()}
-            disabled={username === '' || givenAccessCode === ''}
+            disabled={loginButtonDisables}
           >
             Enter
           </Button>
         </div>
       </div>
-      {errorAlertVisible && (
+      {errorAlertVisible && ( // Render the alert if errorAlertVisible is true
         <Stack sx={{ width: '100%' }} spacing={2}>
           <Alert variant="filled" severity="error">
-            Error: {handleError(errorMessage)}
+            Something went wrong during the login: {handleError(errorMessage)}
           </Alert>
         </Stack>
       )}
