f21ab5ff6bb65a89e760c6848a464ad125852bf5
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-05-24 22:03:26 +0200

    page ws on invite page

--- src/components/views/Invite.js
+++ src/components/views/Invite.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import QRCode from "react-qr-code";
 import {useHistory} from 'react-router-dom';
 import 'styles/views/Invite.scss';
 import Button_Click from "./sounds/Button_Click.mp3";
 import {Container, Button, Typography, Box} from "@mui/material";
-import {getClientDomain} from "../../helpers/getDomain";
+import {getClientDomain, getWebSocketDomain} from "../../helpers/getDomain";
+import Start_Sound from "./sounds/Start_Sound.mp3";
 
 
 const Invite = () => {
     const history = useHistory();
     const accessCode = localStorage.getItem('lobbyAccessCode');
+    const pageWebSocket = useRef(null);
 
     const doBack = () => {
         playSound(Button_Click);
@@ -25,6 +27,35 @@ const Invite = () => {
         playSound(Button_Click);
         navigator.clipboard.writeText(`${getClientDomain()}/user-login/${accessCode}`);
     };
+
+    // Page WebSocket code
+    useEffect(() => {
+        console.log('Opening Page WebSocket');
+        pageWebSocket.current = new WebSocket(getWebSocketDomain() + '/pages/' + accessCode);
+        const openWebSocket = () => {
+            pageWebSocket.current.onopen = (event) => {
+                console.log('Open Page WebSocket:', event);
+            }
+            pageWebSocket.current.onclose = (event) => {
+                console.log('Close Page WebSocket:', event);
+            }
+        }
+        openWebSocket();
+        return () => {
+            console.log('Closing Page WebSocket');
+            pageWebSocket.current.close();
+        }
+    }, []);
+
+    // Page WebSocket code
+    useEffect(() => {
+        pageWebSocket.current.onmessage = (event) => {
+            const IncomingMessage = JSON.parse(event.data);
+            console.log('Received Page Message:', IncomingMessage);
+            playSound(Start_Sound);
+            history.push(IncomingMessage.url);
+        }
+    }, [history]);
 
     return (
             <div className="inviteRoot" style={{overflowY: 'scroll'}}>
