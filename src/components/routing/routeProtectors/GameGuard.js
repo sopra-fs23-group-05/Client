5e4ce767bc655899cf4d46367576d6113d8b84da
tom7689 <tomkaspar.meier@uzh.ch>
2023-05-25 14:42:42 +0200

    cleanup

--- src/components/routing/routeProtectors/GameGuard.js
+++ src/components/routing/routeProtectors/GameGuard.js
@@ -1,34 +1,33 @@
-import { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Redirect} from "react-router-dom";
 import PropTypes from "prop-types";
-import { api } from "../../../helpers/api";
 
 export const GameGuard = (props) => {
-  const [isAuthorized, setIsAuthorized] = useState(null);
+    const [isAuthorized, setIsAuthorized] = useState(null);
 
-  useEffect(() => {
-    const checkAuthorization = async () => {
-      const accessCode = localStorage.getItem("lobbyAccessCode");
-      const url = window.location.href;
-      const urlSplit = url.split("/");
+    useEffect(() => {
+        const checkAuthorization = async () => {
+            const accessCode = localStorage.getItem("lobbyAccessCode");
+            const url = window.location.href;
+            const urlSplit = url.split("/");
 
-      if (urlSplit[4] === accessCode) {
-        setIsAuthorized(true);
-      } else {
-        setIsAuthorized(false);
-      }
-    };
+            if (urlSplit[4] === accessCode) {
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+        };
 
-    checkAuthorization();
-  }, []);
+        checkAuthorization();
+    }, []);
 
-  if (isAuthorized === null) {
-    return <p>Loading...</p>;
-  }
+    if (isAuthorized === null) {
+        return <p>Loading...</p>;
+    }
 
-  return isAuthorized ? props.children : <Redirect to="/homepage" />;
+    return isAuthorized ? props.children : <Redirect to="/homepage"/>;
 };
 
 GameGuard.propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
